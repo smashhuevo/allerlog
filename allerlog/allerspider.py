04f4fb3fac2ad994ea6792b82a1e44daf899297e
Sameer Soleja <sameer.soleja@moleculesoftware.com>
2016-02-18 15:49:23 -0600

    added allergen class, working.

--- allerlog/allerspider.py
+++ allerlog/allerspider.py
@@ -4,6 +4,27 @@ import requests
 import settings
 import pprint
 
+class Allergen:
+
+    def __init__(self, measurement_date, allergen_type, allergen_status, allergen_sum):
+        self.measurement_date = measurement_date
+        self.allergen_type = allergen_type
+        self.allergen_status = allergen_status
+        self.allergen_sum = allergen_sum
+
+
+    measurement_date = None
+
+    allergen_type = None
+    allergen_status = None
+    allergen_sum = None
+
+    allergen_details = None
+
+    def __repr__(self):
+        from pprint import pformat
+        return pformat(vars(self), indent=4, width=1)
+
 class AllerSpider(scrapy.Spider):
     name = 'allerspider'
     start_urls = ['http://www.houstontx.gov/health/Pollen-Mold/']
@@ -23,10 +44,12 @@ class AllerSpider(scrapy.Spider):
         print report_date
 
         main_table = response.selector.xpath('//table[@class="MsoNormalTable"]/tbody/tr/td/div/table/tbody/tr/td/div/text()').extract()
-        print main_table[3:6] # Tree
-        print main_table[6:9] # Weed
-        print main_table[9:12] # Grass
-        print main_table[12:15] # Mold
+
+        tree = Allergen(report_date, main_table[3], main_table[4], main_table[5])
+        weed = Allergen(report_date, main_table[6], main_table[7], main_table[8])
+        grass = Allergen(report_date, main_table[9], main_table[10], main_table[11])
+        mold = Allergen(report_date, main_table[12], main_table[13], main_table[14])
+        print tree, weed, grass, mold
 
 
 
