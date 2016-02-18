1b4225e499d33b93a88f2e045d115504db609ab4
Sameer Soleja <sameer.soleja@moleculesoftware.com>
2016-02-18 16:08:05 -0600

    add jsonpickle

--- allerlog/allerspider.py
+++ allerlog/allerspider.py
@@ -3,6 +3,7 @@ from scrapy.selector import Selector
 import requests
 import settings
 import pprint
+import jsonpickle
 
 class Allergen:
 
@@ -25,6 +26,10 @@ class Allergen:
         from pprint import pformat
         return pformat(vars(self), indent=4, width=1)
 
+    def to_JSON(self):
+        return json.dumps(self, default=lambda o: o.__dict__,
+            sort_keys=True, indent=4)
+
 class AllerSpider(scrapy.Spider):
     name = 'allerspider'
     start_urls = ['http://www.houstontx.gov/health/Pollen-Mold/']
@@ -45,12 +50,11 @@ class AllerSpider(scrapy.Spider):
 
         main_table = response.selector.xpath('//table[@class="MsoNormalTable"]/tbody/tr/td/div/table/tbody/tr/td/div/text()').extract()
 
-        tree = Allergen(report_date, main_table[3], main_table[4], main_table[5])
-        weed = Allergen(report_date, main_table[6], main_table[7], main_table[8])
-        grass = Allergen(report_date, main_table[9], main_table[10], main_table[11])
-        mold = Allergen(report_date, main_table[12], main_table[13], main_table[14])
-        print tree, weed, grass, mold
+        allergens = {'tree': Allergen(report_date, main_table[3], main_table[4], main_table[5]),
+                     'weed': Allergen(report_date, main_table[6], main_table[7], main_table[8]),
+                     'grass': Allergen(report_date, main_table[9], main_table[10], main_table[11]),
+                     'mold': Allergen(report_date, main_table[12], main_table[13], main_table[14])
+        }
 
-
-
-        self.send_main_message(report_date, main_table[3:6])# yield scrapy.Request(response.urljoin(url), self.parse_titles)
+        print allergens
+        self.send_main_message(report_date, jsonpickle.encode(allergens))
